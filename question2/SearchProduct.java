11a8bad126f7b09bb17853da426bffa9717a4bda
ash.sin2982 <ash.sin2982@gmail.com>
2026-07-07 09:17:41 +0530

    Exercise 2: E-commerce Platform Search Function

---
+++ question2/SearchProduct.java
@@ -0,0 +1,45 @@
+package question2;
+
+import java.util.Scanner;
+
+public class SearchProduct {
+
+    public static Product linearSearch(Product[] products, String key) {
+
+        for (Product product : products) {
+            if (product.name.equalsIgnoreCase(key)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+            new Product(101, "Laptop", 65000),
+            new Product(102, "Mobile", 25000),
+            new Product(103, "Headphones", 3000),
+            new Product(104, "Keyboard", 1500),
+            new Product(105, "Mouse", 800)
+        };
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter product name: ");
+        String name = sc.nextLine();
+
+        Product result = linearSearch(products, name);
+
+        if (result != null) {
+            System.out.println("Product Found");
+            System.out.println("ID: " + result.id);
+            System.out.println("Name: " + result.name);
+            System.out.println("Price: ₹" + result.price);
+        } else {
+            System.out.println("Product Not Found");
+        }
+
+        sc.close();
+    }
+}
