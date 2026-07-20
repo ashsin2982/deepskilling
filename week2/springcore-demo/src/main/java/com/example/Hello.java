0e7c16ba0c5ec91905f0f5d807b297683c69a460
ash.sin2982 <ash.sin2982@gmail.com>
2026-07-20 10:44:28 +0530

    Exercise 4: Creating and Configuring a Maven Project

---
+++ week2/springcore-demo/src/main/java/com/example/Hello.java
@@ -0,0 +1,12 @@
+
+
+public package com.example;
+
+public class Hello {
+
+    public void display() {
+        System.out.println("Welcome to Spring Core using Maven!");
+    }
+} Hello {
+    
+}
