1a318a33293decbe19fef385580c84717eae3d58
ash.sin2982 <ash.sin2982@gmail.com>
2026-07-21 21:40:18 +0530

    Create a Spring Web Project using Maven

---
+++ week3/springrest/src/main/java/com/example/springrest/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Spring Boot 3 REST API";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+
+}
