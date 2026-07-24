b6765d39f5cd7c1e9a57aad534a5848aa5562ac9
ash.sin2982 <ash.sin2982@gmail.com>
2026-07-24 22:44:50 +0530

    StudentApp

--- week5/myfirstreact/src/App.js
+++ week5/myfirstreact/src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 function App() {
   return (
     <div>
-      <h1>Welcome to the First Session of React</h1>
+      <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
