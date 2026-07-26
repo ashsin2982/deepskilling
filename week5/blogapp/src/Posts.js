d9a50e6304c90049b6c4da118a835b7fb8238427
ash.sin2982 <ash.sin2982@gmail.com>
2026-07-26 23:06:15 +0530

    react-4

---
+++ week5/blogapp/src/Posts.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Post from "./Post";
+
+class Posts extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => {
+
+                const postList = data.map(item =>
+                    new Post(item.id, item.title, item.body)
+                );
+
+                this.setState({
+                    posts: postList
+                });
+
+            });
+
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>Posts</h1>
+
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    componentDidCatch(error, info) {
+        alert(error);
+    }
+}
+
+export default Posts;
